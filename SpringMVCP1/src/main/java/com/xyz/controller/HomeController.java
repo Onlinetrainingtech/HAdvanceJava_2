9192dd25d4d347991f79b9ba83bf087ed9c957eb
OnlineTraning <57015977+Onlinetrainingtech@users.noreply.github.com>
2023-04-27 07:41:34 +0530

    27-04-2023

---
+++ SpringMVCP1/src/main/java/com/xyz/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.xyz.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+
+public class HomeController
+{
+
+	@RequestMapping("/")
+	public ModelAndView indexPage()
+	{
+		ModelAndView m1=new ModelAndView("index");
+		return m1;
+	}
+}
